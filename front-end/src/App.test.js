8bcb8f46108bc68b570ee82cacdc7931dbffab83
Eliasadam <elias8adam@gmail.com>
2020-11-12 12:14:05 +0000

    updating it with remote branch

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-describe('Hello', () => {
+ describe('Hello', () => {
   it('should contains title', () => {
     const { getByText } = render(<App title="Golden Shoe" />);
     getByText('Golden Shoe');
